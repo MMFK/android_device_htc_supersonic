a7bd2072902f0b22d243b5916761da82097e9c33
Jmz <jmztaylor@gmail.com>
2012-06-11 17:39:05 -0400

    readded fixes

--- full_supersonic.mk
+++ full_supersonic.mk
@@ -22,10 +22,15 @@
 # The gps config appropriate for this device
 PRODUCT_COPY_FILES += \
     device/htc/supersonic/prebuilt/etc/gps.conf:system/etc/gps.conf
+	
+# Market Fix
+PRODUCT_COPY_FILES += \
+    device/htc/supersonic/prebuilt/etc/init.d/99marketfix:system/etc/init.d/99marketfix
 
 PRODUCT_COPY_FILES += \
     device/htc/supersonic/prebuilt/root/init.supersonic.rc:root/init.supersonic.rc \
     device/htc/supersonic/prebuilt/root/init.supersonic.usb.rc:root/init.supersonic.usb.rc \
+	device/htc/supersonic/prebuilt/root/init.rc:root/init.rc \
     device/htc/supersonic/prebuilt/root/ueventd.supersonic.rc:root/ueventd.supersonic.rc 
 
 $(call inherit-product-if-exists, vendor/htc/supersonic/supersonic-vendor.mk)
